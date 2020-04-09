4ac23ac30a68824da770dcb4193aedff438ffb9b
darjanz <4034137-darjanz@users.noreply.gitlab.com>
2020-04-09 17:07:42 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -5,15 +5,57 @@ class Student {
 
 	List<Integer> labPoints = new ArrayList<>();
 
-	//TODO constructor
+	public Student(String index, String firstName, String lastName, List<Integer> labPoints) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.labPoints = labPoints;
+	}
 
-	//TODO seters & getters
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getIndex() {
+		return index;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLabPoints(List<Integer> labPoints) {
+		this.labPoints = labPoints;
+	}
+
+	public List<Integer> getLabPoints() {
+		return labPoints;
+	}
 
 	public double getAverage() {
-		//TODO
+
+		int sum = 0;
+		for (Integer point: labPoints) {
+			sum += point;
+		}
+
+		double avg = (double) sum/labPoints.size();
+		return avg;
 	}
 
 	public boolean hasSignature() {
-		//TODO
+		return labPoints.size() > 8;
 	}
 }
